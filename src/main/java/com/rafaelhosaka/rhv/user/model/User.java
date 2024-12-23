95a369c5cdef2d366c434eb5865233e3162b82d2
rafaelhosaka <hosakarafael@gmail.com>
2024-12-23 16:19:46 +0900

    Add uniqueness to email

--- src/main/java/com/rafaelhosaka/rhv/user/model/User.java
+++ src/main/java/com/rafaelhosaka/rhv/user/model/User.java
@@ -19,6 +19,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+    @Column(unique = true)
     private String email;
     private String imageUrl;
     private Date createdAt;
